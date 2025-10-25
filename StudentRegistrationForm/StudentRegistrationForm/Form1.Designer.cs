5f616bc41fa121c6995839833af386b9b86e965a
aiman <aiababsa123@gmail.com>
2025-10-25 22:10:30 +0300

    Add files via upload

---
+++ StudentRegistrationForm/StudentRegistrationForm/Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace StudentRegistrationForm
+{
+    partial class frmRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.lblBirthdate = new System.Windows.Forms.Label();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.grpGender = new System.Windows.Forms.GroupBox();
+            this.rdoOther = new System.Windows.Forms.RadioButton();
+            this.rdoFemale = new System.Windows.Forms.RadioButton();
+            this.rdoMale = new System.Windows.Forms.RadioButton();
+            this.btnPickColor = new System.Windows.Forms.Button();
+            this.lblSelectedColor = new System.Windows.Forms.Label();
+            this.dtpBirthdate = new System.Windows.Forms.DateTimePicker();
+            this.cmbCountry = new System.Windows.Forms.ComboBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.picStudent = new System.Windows.Forms.PictureBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.grpGender.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lblName.Location = new System.Drawing.Point(30, 30);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(62, 24);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            this.lblName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lblEmail.Location = new System.Drawing.Point(30, 70);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(59, 24);
+            this.lblEmail.TabIndex = 1;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lblPassword.Location = new System.Drawing.Point(30, 110);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(94, 24);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "password";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lblGender.Location = new System.Drawing.Point(30, 150);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(74, 24);
+            this.lblGender.TabIndex = 3;
+            this.lblGender.Text = "Gender";
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lblColor.Location = new System.Drawing.Point(30, 190);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(134, 24);
+            this.lblColor.TabIndex = 4;
+            this.lblColor.Text = "Favorite Color";
+            this.lblColor.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lblBirthdate
+            // 
+            this.lblBirthdate.AutoSize = true;
+            this.lblBirthdate.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lblBirthdate.Location = new System.Drawing.Point(30, 230);
+            this.lblBirthdate.Name = "lblBirthdate";
+            this.lblBirthdate.Size = new System.Drawing.Size(92, 24);
+            this.lblBirthdate.TabIndex = 5;
+            this.lblBirthdate.Text = "Brithdate";
+            this.lblBirthdate.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.AutoSize = true;
+            this.lblCountry.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lblCountry.Location = new System.Drawing.Point(30, 270);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(139, 24);
+            this.lblCountry.TabIndex = 6;
+            this.lblCountry.Text = "Select Country";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lblResult.Location = new System.Drawing.Point(30, 400);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 24);
+            this.lblResult.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(150, 30);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 24);
+            this.txtName.TabIndex = 8;
+            this.txtName.Text = "a";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(150, 70);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(200, 24);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(150, 110);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(200, 24);
+            this.txtPassword.TabIndex = 10;
+            // 
+            // grpGender
+            // 
+            this.grpGender.Controls.Add(this.rdoOther);
+            this.grpGender.Controls.Add(this.rdoFemale);
+            this.grpGender.Controls.Add(this.rdoMale);
+            this.grpGender.Location = new System.Drawing.Point(150, 140);
+            this.grpGender.Name = "grpGender";
+            this.grpGender.Size = new System.Drawing.Size(200, 50);
+            this.grpGender.TabIndex = 11;
+            this.grpGender.TabStop = false;
+            this.grpGender.Text = "Gender";
+            // 
+            // rdoOther
+            // 
+            this.rdoOther.AutoSize = true;
+            this.rdoOther.Location = new System.Drawing.Point(140, 20);
+            this.rdoOther.Name = "rdoOther";
+            this.rdoOther.Size = new System.Drawing.Size(64, 21);
+            this.rdoOther.TabIndex = 2;
+            this.rdoOther.TabStop = true;
+            this.rdoOther.Text = "Other";
+            this.rdoOther.UseVisualStyleBackColor = true;
+            // 
+            // rdoFemale
+            // 
+            this.rdoFemale.AutoSize = true;
+            this.rdoFemale.Location = new System.Drawing.Point(70, 20);
+            this.rdoFemale.Name = "rdoFemale";
+            this.rdoFemale.Size = new System.Drawing.Size(71, 21);
+            this.rdoFemale.TabIndex = 1;
+            this.rdoFemale.TabStop = true;
+            this.rdoFemale.Text = "Female";
+            this.rdoFemale.UseVisualStyleBackColor = true;
+            // 
+            // rdoMale
+            // 
+            this.rdoMale.AutoSize = true;
+            this.rdoMale.Location = new System.Drawing.Point(10, 20);
+            this.rdoMale.Name = "rdoMale";
+            this.rdoMale.Size = new System.Drawing.Size(55, 21);
+            this.rdoMale.TabIndex = 0;
+            this.rdoMale.TabStop = true;
+            this.rdoMale.Text = "Male";
+            this.rdoMale.UseVisualStyleBackColor = true;
+            // 
+            // btnPickColor
+            // 
+            this.btnPickColor.Location = new System.Drawing.Point(150, 190);
+            this.btnPickColor.Name = "btnPickColor";
+            this.btnPickColor.Size = new System.Drawing.Size(75, 23);
+            this.btnPickColor.TabIndex = 12;
+            this.btnPickColor.Text = "Choose Color";
+            this.btnPickColor.UseVisualStyleBackColor = true;
+            this.btnPickColor.Click += new System.EventHandler(this.btnPickColor_Click_1);
+            // 
+            // lblSelectedColor
+            // 
+            this.lblSelectedColor.AutoSize = true;
+            this.lblSelectedColor.Location = new System.Drawing.Point(300, 190);
+            this.lblSelectedColor.Name = "lblSelectedColor";
+            this.lblSelectedColor.Size = new System.Drawing.Size(128, 17);
+            this.lblSelectedColor.TabIndex = 13;
+            this.lblSelectedColor.Text = "\"No Color Selected\"";
+            // 
+            // dtpBirthdate
+            // 
+            this.dtpBirthdate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpBirthdate.Location = new System.Drawing.Point(150, 230);
+            this.dtpBirthdate.Name = "dtpBirthdate";
+            this.dtpBirthdate.Size = new System.Drawing.Size(200, 24);
+            this.dtpBirthdate.TabIndex = 14;
+            // 
+            // cmbCountry
+            // 
+            this.cmbCountry.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCountry.FormattingEnabled = true;
+            this.cmbCountry.Items.AddRange(new object[] {
+            "Yemen",
+            "Egypt",
+            "Oman",
+            "Qatar",
+            "Palestine",
+            "Syria"});
+            this.cmbCountry.Location = new System.Drawing.Point(175, 270);
+            this.cmbCountry.Name = "cmbCountry";
+            this.cmbCountry.Size = new System.Drawing.Size(121, 24);
+            this.cmbCountry.TabIndex = 15;
+            this.cmbCountry.SelectedIndexChanged += new System.EventHandler(this.cmbCountry_SelectedIndexChanged);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(150, 320);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(100, 30);
+            this.btnSubmit.TabIndex = 16;
+            this.btnSubmit.Text = "Register";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(260, 320);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 30);
+            this.btnReset.TabIndex = 17;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // picStudent
+            // 
+            this.picStudent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picStudent.Location = new System.Drawing.Point(400, 30);
+            this.picStudent.Name = "picStudent";
+            this.picStudent.Size = new System.Drawing.Size(120, 120);
+            this.picStudent.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picStudent.TabIndex = 18;
+            this.picStudent.TabStop = false;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(400, 160);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(120, 30);
+            this.btnUpload.TabIndex = 19;
+            this.btnUpload.Text = "Upload Picture";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(260, 360);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 30);
+            this.btnSave.TabIndex = 20;
+            this.btnSave.Text = "Save Data";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(380, 360);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(100, 30);
+            this.btnLoad.TabIndex = 21;
+            this.btnLoad.Text = "Laod Data";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // frmRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(582, 453);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.picStudent);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.cmbCountry);
+            this.Controls.Add(this.dtpBirthdate);
+            this.Controls.Add(this.lblSelectedColor);
+            this.Controls.Add(this.btnPickColor);
+            this.Controls.Add(this.grpGender);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblCountry);
+            this.Controls.Add(this.lblBirthdate);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblName);
+            this.Name = "frmRegistration";
+            this.RightToLeftLayout = true;
+            this.Text = "Student Registration Form";
+            this.Load += new System.EventHandler(this.frmRegistration_Load);
+            this.grpGender.ResumeLayout(false);
+            this.grpGender.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.Label lblBirthdate;
+        private System.Windows.Forms.Label lblCountry;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.GroupBox grpGender;
+        private System.Windows.Forms.RadioButton rdoOther;
+        private System.Windows.Forms.RadioButton rdoFemale;
+        private System.Windows.Forms.RadioButton rdoMale;
+        private System.Windows.Forms.Button btnPickColor;
+        private System.Windows.Forms.Label lblSelectedColor;
+        private System.Windows.Forms.DateTimePicker dtpBirthdate;
+        private System.Windows.Forms.ComboBox cmbCountry;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.PictureBox picStudent;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
